66d38253de88e747bb7833933323651660d04021
Igor Silvestre <77891078+IgorSilvestre@users.noreply.github.com>
2022-03-16 15:11:57 -0300

    config database file and imported in index.js

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const app = express()
-
 const db = require('./src/database_utils')
-db.connect(app)
 
 const PORT = 3000
+app.get ('/', (req, res, next) => {
+    db.start()
+    db.getFrom(res, '*', 'users')
+})
 
-app.listen (PORT, () => {
-    
+app.listen (PORT, () => {  
     console.log(`Server running on localhost:${PORT}`)
 })
--- src/database_utils.js
+++ src/database_utils.js
@@ -1,22 +1,23 @@
 const { Client } = require('pg');
-const client = require('pg/lib/native/client');
+const start= () => {
+    db = new Client({
+        host: 'localhost',
+        user: '@',
+        port: 5432,
+        password: '@',
+        database: '@'
+    })
+    db.connect()
+}
 
-const client = new Client({
-    host: 'localhost',
-    user: 'igorsilvestre',
-    port: 5432,
-    password: 'll -',
-    database: 'naovoumais'
-})
-client.connect()
-export default client
+const getFrom = (res, selected, table) => {
+    db.query(`SELECT ${selected} FROM public.${table}`, (err, result) => {
+        if (err) {
+            console.log(err);
+            res.status(400).send(err);
+        }
+        res.status(200).send(result.rows);
+    })
+}
 
-// app.get('/', function (req, res, next) {
-//     client.query('SELECT * FROM Employee where id = $1', [1], function (err, result) {
-//         if (err) {
-//             console.log(err);
-//             res.status(400).send(err);
-//         }
-//         res.status(200).send(result.rows);
-//     });
-// });
+module.exports = { start, getFrom }
