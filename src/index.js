1ecde30f92986146fb659cf014145e4880803924
Igor Silvestre <77891078+IgorSilvestre@users.noreply.github.com>
2022-03-18 16:03:58 -0300

    changed folder structure

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const db = require('./src/database_utils')
+const db = require('./controller/database_utils')
 const PORT = 3000
 
 
@@ -9,14 +9,14 @@ const PORT = 3000
 app.get ('/', (req, res) => {
     // db.start()
     // db.getFrom(res, '*', 'users')
-    res.sendFile(path.join('/index.html'))
+    res.sendFile(path.join('/public/index.html'))
 })
 
 // LOGIN PAGE
 app.get('/login', (req, res) => {
     // app.use(express.static('/utils', path.join(__dirname + '/routes/login.html')))
     console.log(req)
-    res.render('/routes/login.html')
+    res.render('/src/routes/login.html')
 })
 
 
