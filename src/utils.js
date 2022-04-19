1a0de5fc7bb253f9186fead941273d71aa4db3aa
Lucas da Silva <sklucassilva@gmail.com>
2022-04-19 19:46:29 -0300

    Fixed identation

--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,10 @@
 const crypto = require('crypto')
 
-function generatePasswordSalt () {
+function generatePasswordSalt() {
     return Math.random().toString(8)
 }
 
-function sha512(password, salt){
+function sha512(password, salt) {
     var hash = crypto.createHmac('sha512', salt); // sha512 crypto algorithm
     hash.update(password);
     var hash = hash.digest('hex')
@@ -17,6 +17,6 @@ function sha512(password, salt){
 function authenticatePassword(loginPassword, hashOnDb, saltOnDb) {
     let attemptPassword = sha512(loginPassword, saltOnDb)
     return hashOnDb === attemptPassword.hash
- }
+}
 
 module.exports = { generatePasswordSalt, sha512, authenticatePassword }
